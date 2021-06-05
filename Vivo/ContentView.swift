22d9fda631bd33fe2d02c5c370cdb2572e80b324
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-05 09:52:40 -0300

    Initial Commit

---
+++ Vivo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vivo
+//
+//  Created by Raphael Cerqueira on 05/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vivo/VivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VivoApp.swift
+//  Vivo
+//
+//  Created by Raphael Cerqueira on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct VivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
